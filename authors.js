228a525f0f89a4de33f09db316b34c46ec883d2b
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-18 23:05:15 +0200

    add authors

---
+++ authors.js
@@ -0,0 +1,32 @@
+export default [
+  /**
+   * @bashmish, 11 May 2015
+   * Майские каникулы, 04 May 2015
+   * @sudodoki, 27 Apr 2015
+   * @pukhalski, 20 Apr 2015
+   * @lomakin, 13 Apr 2015
+   * @mraleph, 06 Apr 2015
+   * @AWinogradov, 30 Mar 2015
+   * @veged, 23 Mar 2015
+   * @korochinskiy, 16 Mar 2015
+   * @toivonens, 09 Mar 2015
+   * @roman01la, 02 Mar 2015
+   * @matmuchrapna, 23 Feb 2015
+   * Провал, 16 Feb 2015
+   * @filipovskii, 09 Feb 2015
+   * @shuvalov_anton, 03 Feb 2015
+   */
+  { start: '597653605315977216', tweets: [], username: 'bashmish' },
+  { start: '592841474334658560', tweets: [], username: 'sudodoki' },
+  { start: '590058545422409728', tweets: [], username: 'pukhalski' },
+  { start: '587596754524188672', tweets: [], username: 'lomakin' },
+  { start: '584858429212258304', tweets: [], username: 'mraleph' },
+  { start: '582443238063251456', tweets: [], username: 'AWinogradov' },
+  { start: '579986868487512064', tweets: [], username: 'veged' },
+  { start: '577403514445500416', tweets: [], username: 'korochinskiy' },
+  { start: '574884457691418624', tweets: [], username: 'toivonens' },
+  { start: '572341652246945792', tweets: [], username: 'roman01la' },
+  { start: '569802366297493505', tweets: [], username: 'matmuchrapna' },
+  { start: '564697062378594304', tweets: [], username: 'filipovskii' },
+  { start: '562516792753811456', tweets: [], username: 'shuvalov_anton' },
+];
