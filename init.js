d7b886324bdb1a5ddb79f7e44437548dd349ec2a
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-19 02:06:26 +0200

    add init++

--- init.js
+++ init.js
@@ -9,7 +9,7 @@ const tokens = {
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 };
 
-const dump = (authors, dump)=> {
+const dump = (authors, tweets)=> {
   // separate tweets
   authors.forEach((author)=> {
     const number = tweets.findIndex(item=> item.id_str === author.start) + 1;
@@ -18,7 +18,9 @@ const dump = (authors, dump)=> {
   });
 
   // save tweets
-  authors.forEach(author => fs.outputJson(`./dump/${author.username}.json`, author);
+  authors.forEach(author => {
+    fs.outputJson(`./dump/${author.username}.json`, author, (err)=> console.log(`${author.username} done`))
+  });
 }
 
 getTweets(tokens, 'jsunderhood').then(tweets => dump(authors, tweets.items));
