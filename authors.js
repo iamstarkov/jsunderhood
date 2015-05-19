e1162d83fe69e8b52c9cef298c08f7dc16736e92
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-19 02:06:48 +0200

    add subzey to authors

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 export default [
   /**
+   * @subzey, 18 May 2015
    * @bashmish, 11 May 2015
    * Майские каникулы, 04 May 2015
    * @sudodoki, 27 Apr 2015
@@ -16,17 +17,18 @@ export default [
    * @filipovskii, 09 Feb 2015
    * @shuvalov_anton, 03 Feb 2015
    */
-  { start: '597653605315977216', tweets: [], username: 'bashmish' },
-  { start: '592841474334658560', tweets: [], username: 'sudodoki' },
-  { start: '590058545422409728', tweets: [], username: 'pukhalski' },
-  { start: '587596754524188672', tweets: [], username: 'lomakin' },
-  { start: '584858429212258304', tweets: [], username: 'mraleph' },
-  { start: '582443238063251456', tweets: [], username: 'AWinogradov' },
-  { start: '579986868487512064', tweets: [], username: 'veged' },
-  { start: '577403514445500416', tweets: [], username: 'korochinskiy' },
-  { start: '574884457691418624', tweets: [], username: 'toivonens' },
-  { start: '572341652246945792', tweets: [], username: 'roman01la' },
-  { start: '569802366297493505', tweets: [], username: 'matmuchrapna' },
-  { start: '564697062378594304', tweets: [], username: 'filipovskii' },
-  { start: '562516792753811456', tweets: [], username: 'shuvalov_anton' },
+  { username: 'subzey',         start: '600095365254270976' },
+  { username: 'bashmish',       start: '597653605315977216' },
+  { username: 'sudodoki',       start: '592841474334658560' },
+  { username: 'pukhalski',      start: '590058545422409728' },
+  { username: 'lomakin',        start: '587596754524188672' },
+  { username: 'mraleph',        start: '584858429212258304' },
+  { username: 'AWinogradov',    start: '582443238063251456' },
+  { username: 'veged',          start: '579986868487512064' },
+  { username: 'korochinskiy',   start: '577403514445500416' },
+  { username: 'toivonens',      start: '574884457691418624' },
+  { username: 'roman01la',      start: '572341652246945792' },
+  { username: 'matmuchrapna',   start: '569802366297493505' },
+  { username: 'filipovskii',    start: '564697062378594304' },
+  { username: 'shuvalov_anton', start: '562516792753811456' },
 ];
