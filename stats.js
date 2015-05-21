8651d5d716334584aa68d9bd3c706288b8e9cdb6
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-21 20:57:30 +0200

    add output to stats scripts

--- stats.js
+++ stats.js
@@ -6,19 +6,16 @@ const isReply = (tweet)=> tweet.in_reply_to_screen_name;
 
 const analyze = (author)=> {
 	const username = author.username;
-	const tweets = {
-		total: author.tweets.length,
-		retweets: author.tweets.filter(isRetweet).length,
-		replies: author.tweets.filter(isReply).length
-	};
+	const total = author.tweets.length;
+	const retweets = author.tweets.filter(isRetweet).length;
+	const replies = author.tweets.filter(isReply).length;
 	const retweeted = author.tweets.reduce((state, tweet)=> state + tweet.retweet_count, 0);
 	const favorited = author.tweets.reduce((state, tweet)=> state + tweet.favorite_count, 0);
-	const retweetedPerTweet = (tweets.total / retweeted).toFixed(2);
+	const retweetedPerTweet = (total / retweeted).toFixed(2);
 
-
-	console.log(
-`${username} wrote ${tweets.total} tweets, been retweeted ${retweeted} and favorited ${favorited} times. RTed/Tweet: ${retweetedPerTweet}`
-	)
+	fs.outputJson(`dump/${author.username}-stats.json`, {
+		username, total, retweets, replies, retweeted, favorited, retweetedPerTweet
+	}, (err)=> console.log(`${author.username} done`));
 }
 
 authors.forEach((item)=> {
