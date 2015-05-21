8522005c61a61f331c13bf940525120207dec189
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-21 07:15:19 +0200

    add followers-stats script

--- followers.js
+++ followers.js
@@ -1,18 +1,40 @@
 import fs from 'fs-extra';
 import moment from 'moment';
-import authors from './authors';
-import { date as rawDump } from './followers-dump.json';
+import authorsRaw from './authors';
+import { date as rawDump } from './followers-stats-raw.json';
 
 const log = console.log.bind(console);
 const f = (input)=> new Date(input).getTime();
 
-const dump = rawDump.map((point)=> {
-	return { count: point.count, date: moment(point.value, 'YYYY-MM-DD').unix() };
-}).sort((a, b)=> a.date - b.date);
+const first = (arr)=> arr[0];
+const last = (arr)=> arr[arr.length - 1];
 
-authors.forEach((author)=> {
-	author.start = moment(author.start, 'DD MMM YYYY').unix();
-	const startPoint = dump.find((point) => point.date === author.start);
+const dump = rawDump
+	.map((point)=> {
+		return {
+			count: point.count,
+			date: moment(point.value, 'YYYY-MM-DD').unix(),
+			_date: point.value
+		};
+	})
+	.sort((a, b)=> a.date - b.date);
 
-	log(author.username, startPoint);
-});
+const authors = authorsRaw
+	.map((author)=> {
+		return {
+			username: author.username,
+			start: moment(author.start, 'DD MMM YYYY').unix(),
+			_start: author.start
+		};
+	})
+	.sort((a, b)=> a.start - b.start)
+	.map((author, i, arr)=> {
+		author.startCount = last(dump.filter((point)=> point.date <= author.start)).count;
+		return author;
+	})
+	.map((author, i, arr)=> {
+		author.gainedCount = arr[i+1] ? (arr[i+1].startCount - author.startCount) : 0;
+		return author;
+	});
+
+fs.outputJson(`./followers-stats.json`, authors, (err)=> console.log(`done`))
