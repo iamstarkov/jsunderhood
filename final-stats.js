da00263dd4c05bce9d8471695166c488571b7324
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-24 07:56:36 +0200

    add stars to stats

--- final-stats.js
+++ final-stats.js
@@ -1,12 +1,18 @@
 import authorsRaw from './authors.js'
 import fs from 'fs-extra';
+import r from 'ramda';
 import assign from 'object-assign';
 import moment from 'moment';
 import gainedFollowers from './followers-stats.json'
 
+const first = (arr)=> arr[0];
+const last = (arr)=> arr[arr.length - 1];
+const sortBy = (field)=> (a, b)=> a[field] - b[field];
 const log = console.log.bind(console);
 
-const stats = authorsRaw
+
+
+let stats = authorsRaw
 	.map((author)=> {
 		return {
 			username: author.username,
@@ -26,5 +32,17 @@ const stats = authorsRaw
 		author.gainedFollowers = gainedFollowers[author.username];
 		return author;
 	});
+
+
+r.maxBy(r.prop('tweets'), stats).maxTweets = true;
+r.maxBy(r.prop('ownTweets'), stats).maxOwnTweets = true;
+r.maxBy(r.prop('ownTweetsPercentage'), stats).maxOwnTweetsPercentage = true;
+r.maxBy(r.prop('gainedFollowers'), stats).maxGainedFollowers = true;
+r.maxBy(r.prop('retweets'), stats).maxRetweets = true;
+r.maxBy(r.prop('retweetsPercentage'), stats).maxRetweetsPercentage = true;
+r.maxBy(r.prop('replies'), stats).maxReplies = true;
+r.maxBy(r.prop('repliesPercentage'), stats).maxRepliesPercentage = true;
+r.maxBy(r.prop('favoritedKpi'), stats).maxFavoritedKpi = true;
+r.maxBy(r.prop('retweetedKpi'), stats).maxRetweetedKpi = true;
 
 fs.outputJson(`./final-stats.json`, {stats}, (err)=> console.log(`done`))
