28feb0cf9f76549271a8ca1c921ced182f4b1470
Vladimir Starkov <iamstarkov@gmail.com>
2015-05-31 17:20:18 +0200

    extract tokens to separate file

---
+++ tokens.js
@@ -0,0 +1,6 @@
+export default {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+};
