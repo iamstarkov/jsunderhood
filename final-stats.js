ebc7422887046141a36b8d1c24ccc81b85bffe46
Vladimir Starkov <iamstarkov@gmail.com>
2015-06-01 03:00:47 +0200

    update final-stats

--- final-stats.js
+++ final-stats.js
@@ -9,28 +9,30 @@ const first = (arr)=> arr[0];
 const last = (arr)=> arr[arr.length - 1];
 const sortBy = (field)=> (a, b)=> a[field] - b[field];
 
-
-
 let stats = authorsRaw
-	.map((author)=> {
-		return {
-			username: author.username,
-			start: moment(author.start, 'DD MMM YYYY').unix(),
-		};
-	})
-	.sort((a, b)=> a.start - b.start)
-	.map((author, i)=> {
-		return { username: author.username, index: i+1 };
-	})
-	.map((author)=> {
-		var stats = fs.readJsonSync(`./dump/${author.username}-stats.json`);
-		delete stats.username;
-		return assign({}, author, stats);
-	})
-	.map((author)=> {
-		author.gainedFollowers = gainedFollowers[author.username];
-		return author;
-	});
+  .map((author)=> {
+    return {
+      username: author.username,
+      start: moment(author.start, 'DD MMM YYYY').unix(),
+    };
+  })
+  .sort((a, b)=> a.start - b.start)
+  .map((author, i)=> {
+    return { username: author.username, index: i+1 };
+  })
+  .map((author, i, arr)=> {
+    var stats = fs.readJsonSync(`./dump/${author.username}-stats.json`);
+    author.followers_count = fs.readJsonSync(`./dump/${author.username}-info.json`).followers_count;
+    return assign({}, author, stats);
+  })
+  .map((author, i, arr)=> {
+    author.gainedFollowers2 = arr[i-1] ? (author.followers_count - arr[i-1].followers_count) : author.followers_count;
+    return author;
+  })
+  .map((author)=> {
+    author.gainedFollowers = gainedFollowers[author.username];
+    return author;
+  });
 
 
 r.maxBy(r.prop('tweets'), stats).maxTweets = true;
