e31d85def9a82ace0beaef68aa3bd4d52306bb89
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-02 04:19:38 +0200

    small fixes to final-stats.js

--- final-stats.js
+++ final-stats.js
@@ -4,30 +4,26 @@ import r from 'ramda';
 import assign from 'object-assign';
 import moment from 'moment';
 
-const first = (arr)=> arr[0];
-const last = (arr)=> arr[arr.length - 1];
-const sortBy = (field)=> (a, b)=> a[field] - b[field];
+const first = arr => arr[0];
+const last = arr => arr[arr.length - 1];
+const sortBy = field => (a, b) => a[field] - b[field];
 
 let stats = authorsRaw
-  .map((author)=> {
-    return {
-      username: author.username,
-      start: moment(author.start, 'DD MMM YYYY').unix(),
-    };
-  })
-  .sort((a, b)=> a.start - b.start)
-  .map((author, i)=> {
-    return { username: author.username, index: i+1 };
-  })
-  .map((author, i, arr)=> {
+  .map(author => ({
+    username: author.username,
+    start: moment(author.start, 'DD MMM YYYY').unix()
+  }))
+  .sort((a, b) => a.start - b.start)
+  .map((author, i) => ({ username: author.username, index: i + 1 }))
+  .map((author, i, arr) => {
     var stats = fs.readJsonSync(`./dump/${author.username}-stats.json`);
     author.followers_count = fs.readJsonSync(`./dump/${author.username}-info.json`).followers_count;
     return assign({}, author, stats);
   })
-  .map((author, i, arr)=> {
+  .map((author, i, arr) => {
     author.gainedFollowers = arr[i-1] ? (author.followers_count - arr[i-1].followers_count) : author.followers_count;
     return author;
-  })
+  });
 
 
 r.maxBy(r.prop('tweets'), stats).maxTweets = true;
@@ -43,4 +39,4 @@ r.maxBy(r.prop('favoritedKpi'), stats).maxFavoritedKpi = true;
 r.maxBy(r.prop('retweeted'),    stats).maxRetweeted = true;
 r.maxBy(r.prop('retweetedKpi'), stats).maxRetweetedKpi = true;
 
-fs.outputJson(`./final-stats.json`, {stats}, (err)=> console.log(`done`))
+fs.outputJson(`./final-stats.json`, { stats }, (err) => console.log(`done`));
