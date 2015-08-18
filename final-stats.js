f582cdcce9cd39a0e08c562af87f3a5afc5a40e4
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-19 00:17:06 +0200

    simplify stats flow

--- final-stats.js
+++ final-stats.js
@@ -3,6 +3,7 @@ import fs from 'fs-extra';
 import r from 'ramda';
 import assign from 'object-assign';
 import moment from 'moment';
+import twStats from 'tweets-stats';
 
 let stats = authorsRaw
   .map(author => ({
@@ -12,27 +13,23 @@ let stats = authorsRaw
   .sort((a, b) => a.start - b.start)
   .map((author, i) => ({ username: author.username, index: i + 1 }))
   .map((author, i, arr) => {
-    var stats = fs.readJsonSync(`./dump/${author.username}-stats.json`);
+    const tweets = fs.readJsonSync(`./dump/${author.username}.json`).tweets;
     author.followers_count = fs.readJsonSync(`./dump/${author.username}-info.json`).followers_count;
-    return assign({}, author, stats);
+    return assign({}, author, twStats(tweets));
   })
   .map((author, i, arr) => {
     author.gainedFollowers = arr[i-1] ? (author.followers_count - arr[i-1].followers_count) : author.followers_count;
     return author;
   });
 
-
-r.maxBy(r.prop('tweets'), stats).maxTweets = true;
-r.maxBy(r.prop('ownTweets'), stats).maxOwnTweets = true;
-r.maxBy(r.prop('ownTweetsPercentage'), stats).maxOwnTweetsPercentage = true;
-r.maxBy(r.prop('gainedFollowers'), stats).maxGainedFollowers = true;
-r.maxBy(r.prop('retweets'), stats).maxRetweets = true;
-r.maxBy(r.prop('retweetsPercentage'), stats).maxRetweetsPercentage = true;
-r.maxBy(r.prop('replies'), stats).maxReplies = true;
-r.maxBy(r.prop('repliesPercentage'), stats).maxRepliesPercentage = true;
-r.maxBy(r.prop('favorited'),    stats).maxFavorited = true;
-r.maxBy(r.prop('favoritedKpi'), stats).maxFavoritedKpi = true;
-r.maxBy(r.prop('retweeted'),    stats).maxRetweeted = true;
-r.maxBy(r.prop('retweetedKpi'), stats).maxRetweetedKpi = true;
+r.maxBy(r.prop('tweets'),            stats).maxTweets = true;
+r.maxBy(r.prop('gainedFollowers'),   stats).maxGainedFollowers = true;
+r.maxBy(r.path('own.total'.split('.')), stats).maxOwn = true;
+r.maxBy(r.path('replies.total'.split('.')),     stats).maxReplies  = true;
+r.maxBy(r.path('retweets.total'.split('.')),    stats).maxRetweets = true;
+r.maxBy(r.path('favorited.total'.split('.')),   stats).maxFavorited        = true;
+r.maxBy(r.path('favorited.average'.split('.')), stats).maxFavoritedAverage = true;
+r.maxBy(r.path('retweeted.total'.split('.')),   stats).maxRetweeted         = true;
+r.maxBy(r.path('retweeted.average'.split('.')), stats).maxRetweetedAverage  = true;
 
 fs.outputJson(`./final-stats.json`, { stats }, (err) => console.log(`done`));
