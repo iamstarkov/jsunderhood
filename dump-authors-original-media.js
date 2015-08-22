563bfd3db61f4bde2d48d61e9ff04219dcce50ae
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-22 03:25:14 +0200

    finish dump-authors-original-media

--- dump-authors-original-media.js
+++ dump-authors-original-media.js
@@ -1,4 +1,4 @@
-import fs from 'fs-extra'
+import { mkdirs, createWriteStream } from 'fs-extra'
 import got from 'got';
 import media from 'twitter-profile-media';
 import tokens from 'twitter-tokens';
@@ -6,15 +6,18 @@ import authors from './authors';
 
 const load = (imageUrl, targetFilename) =>
   got.stream(imageUrl, { encoding: null })
-    .pipe(fs.createWriteStream(targetFilename))
+    .pipe(createWriteStream(targetFilename))
     .on('finish', () => { console.log(`${targetFilename} finish`) });
 
-authors
-  .map(author => author.username)
-  .forEach(author => {
-    media(tokens, author, (err, { image, banner }) => {
-      if (err) throw err;
-      image &&  load(image,  `dump/images/original/${author}-image.jpg`);
-      banner && load(banner, `dump/images/original/${author}-banner.png`);
+mkdirs('dump/images/original', (err) => {
+  if (err) throw err;
+  authors
+    .map(author => author.username)
+    .forEach(author => {
+      media(tokens, author, (err, { image, banner }) => {
+        if (err) throw err;
+        image &&  load(image,  `dump/images/original/${author}-image.jpg`);
+        banner && load(banner, `dump/images/original/${author}-banner.png`);
+      });
     });
-  })
+});
