916cd8e4208321a51d3d932a783579eed6e9d912
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-22 04:36:14 +0200

    simplify it a bit

--- dump-authors-original-media.js
+++ dump-authors-original-media.js
@@ -1,23 +1,4 @@
-import { mkdirs, createWriteStream } from 'fs-extra'
-import got from 'got';
-import media from 'twitter-profile-media';
-import tokens from 'twitter-tokens';
 import authors from './authors';
+import loadMedia from './helpers/load-media';
 
-const load = (imageUrl, targetFilename) =>
-  got.stream(imageUrl, { encoding: null })
-    .pipe(createWriteStream(targetFilename))
-    .on('finish', () => { console.log(`${targetFilename} finish`) });
-
-mkdirs('dump/images/original', (err) => {
-  if (err) throw err;
-  authors
-    .map(author => author.username)
-    .forEach(author => {
-      media(tokens, author, (err, { image, banner }) => {
-        if (err) throw err;
-        image &&  load(image,  `dump/images/original/${author}-image.jpg`);
-        banner && load(banner, `dump/images/original/${author}-banner.png`);
-      });
-    });
-});
+authors.forEach(author => { loadMedia(author.username) });
