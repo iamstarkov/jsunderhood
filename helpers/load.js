0c02a8158ac929cd8b55e8fe05180a939a7bc287
Leonard Kinday <leonard@kinday.ru>
2015-08-22 21:08:28 +0300

    handle errors

--- helpers/load.js
+++ helpers/load.js
@@ -4,12 +4,18 @@ import mime from 'mime';
 import magic from 'stream-mmmagic';
 
 export default function load(imageUrl, targetName) {
-  const input = got.stream(imageUrl, { encoding: null });
-  return magic(input, (err, { type }, output) => {
-    // `mime` uses `jpeg` as extension for `image/jpeg`
-    const targetExt = (type === 'image/jpeg') ? 'jpg' : mime.extension(type);
-    const targetFilename = `${targetName}.${targetExt}`;
-    output.pipe(createWriteStream(targetFilename))
-    .on('finish', () => { console.log(`${targetFilename} finish`) });
+  got.stream(imageUrl, { encoding: null })
+  .on('response', response => {
+    if (response.statusCode >= 400) {
+      console.log(`✖ Error ${response.statusCode} for ${targetName}`);
+      return;
+    }
+    magic(response, (err, { type }, output) => {
+      // `mime` uses `jpeg` as extension for `image/jpeg`
+      const targetExt = (type === 'image/jpeg') ? 'jpg' : mime.extension(type);
+      const targetFilename = `${targetName}.${targetExt}`;
+      output.pipe(createWriteStream(targetFilename))
+      .on('finish', () => { console.log(`✔ ${targetFilename} finish`) });
+    });
   });
 }
