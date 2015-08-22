f9de6631c9ccfb78fc915f183cf9c29b16b39ba9
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-22 04:50:21 +0200

    add dump current author images functionality

--- update.js
+++ update.js
@@ -2,17 +2,20 @@ import fs from 'fs-extra';
 import getTweets from 'get-tweets';
 import tokens from 'twitter-tokens';
 import authors from './authors';
+import loadMedia from './helpers/load-media';
 
 const log = console.log.bind(null);
 const author = authors[0];
 
+loadMedia('jsunderhood', 'dump/images', author.username);
+
 getTweets(tokens, 'jsunderhood', author.first, (err, tweets, missed, info) => {
-	if (err) throw err;
-	author.tweets = tweets;
-	fs.outputJson(`./dump/${author.username}.json`, author, err =>
-		log(`${author.username} done`));
-	fs.outputJson(`./dump/${author.username}-info.json`, info, err =>
-		log(`${author.username} info done`));
-	fs.outputJson(`./dump/latest-info.json`, info, err =>
-		log(`latest info done`));
+  if (err) throw err;
+  author.tweets = tweets;
+  fs.outputJson(`./dump/${author.username}.json`, author, err =>
+    log(`${author.username} done`));
+  fs.outputJson(`./dump/${author.username}-info.json`, info, err =>
+    log(`${author.username} info done`));
+  fs.outputJson(`./dump/latest-info.json`, info, err =>
+    log(`latest info done`));
 });
