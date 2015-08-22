aee097ab648e1bc00f9488efbe7fcf831ed904ab
Leonard Kinday <leonard@kinday.ru>
2015-08-22 20:11:31 +0300

    detect files extensions using MIME-type

--- helpers/load.js
+++ helpers/load.js
@@ -1,8 +1,15 @@
 import { createWriteStream } from 'fs-extra';
 import got from 'got';
+import mime from 'mime';
+import magic from 'stream-mmmagic';
 
-export default function load(imageUrl, targetFilename) {
-  return got.stream(imageUrl, { encoding: null })
-    .pipe(createWriteStream(targetFilename))
+export default function load(imageUrl, targetName) {
+  const input = got.stream(imageUrl, { encoding: null });
+  return magic(input, (err, { type }, output) => {
+    // `mime` uses `jpeg` as extension for `image/jpeg`
+    const targetExt = (type === 'image/jpeg') ? 'jpg' : mime.extension(type);
+    const targetFilename = `${targetName}.${targetExt}`;
+    output.pipe(createWriteStream(targetFilename))
     .on('finish', () => { console.log(`${targetFilename} finish`) });
+  });
 }
