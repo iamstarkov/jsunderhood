71169355a2a5e1e8a716b07e415840f4deaed3d3
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2015-09-07 10:01:24 +0200

    add @__fro

--- authors.js
+++ authors.js
@@ -3,6 +3,7 @@ import fs from 'fs-extra';
 
 export default [
   // post: false
+  { username: '__fro',           start: '07 Sep 2015', first: '640793415169536000', post: false },
   { username: 'RReverser',       start: '31 Aug 2015', first: '638181485497991168' },
   { username: 'neonick',         start: '24 Aug 2015', first: '635790657156685824' },
   { username: 'pepelsbey',       start: '17 Aug 2015', first: '633203477540380672' },
