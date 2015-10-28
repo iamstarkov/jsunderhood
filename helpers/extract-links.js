f7288936b1c22683ba2a44d3083a12a84895b7cf
Vladimir Starkov <iamstarkov@gmail.com>
2015-10-28 21:38:18 +0100

    proper links extraction

--- helpers/extract-links.js
+++ helpers/extract-links.js
@@ -1,17 +1,32 @@
 import tweetLinks from 'tweet-links';
-import author from '../dump/edjafarov.json'
-import { map, compose, reverse, filter, sort, flatten } from 'ramda';
+import { sortBy, nthArg, prop, head, keys, apply, objOf, toPairs, pipe, length, reduce, mapObjIndexed, groupBy, map, compose, reverse, filter, sort, flatten, uniq } from 'ramda';
+import { parse, format } from 'url';
 
-const log = input => {
-  console.log(input);
-  return input
-}
-const getLinks = compose(
-  flatten,
-  map(tweetLinks)
+const initial = pipe(map(tweetLinks), flatten, uniq);
+const notTwitter = url => url.host !== 'twitter.com';
+const moveAllMinorsToOther = group => (length(group.links) < 5) ? 'other' : group.host;
+const groupByHost = pipe(
+  groupBy(prop('host')),
+  mapObjIndexed((value, key) => ({ host: key, links: value })),
+  compose(map(apply(objOf)), toPairs),
+  map(i => prop(head(keys(i)), i))
+);
+const constructResultObject = pipe(
+  mapObjIndexed(map(prop('links'))),
+  mapObjIndexed(flatten),
+  mapObjIndexed(map(format)), // format links
+  toPairs,
+  map(i => ({ host: i[0], links: i[1] }))
 );
 
-const res = getLinks(author.tweets);
-// console.log(res);
+const getLinks = pipe(
+  initial,
+  map(parse), // parse links
+  filter(notTwitter),
+  groupByHost,
+  groupBy(moveAllMinorsToOther),
+  constructResultObject,
+  sortBy(group => group.host === 'other')
+);
 
 export default getLinks;
