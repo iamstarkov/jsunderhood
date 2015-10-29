702ed6a9abc2b61723afd6aa27ee8480281c4265
Vladimir Starkov <iamstarkov@gmail.com>
2015-10-29 12:49:02 +0100

    even more better getLinks

--- helpers/extract-links.js
+++ helpers/extract-links.js
@@ -1,32 +1,16 @@
 import tweetLinks from 'tweet-links';
-import { sortBy, nthArg, prop, head, keys, apply, objOf, toPairs, pipe, length, reduce, mapObjIndexed, groupBy, map, compose, reverse, filter, sort, flatten, uniq } from 'ramda';
+import { zipObj, values, sortBy, prop, head, apply, objOf, toPairs, pipe, length, mapObjIndexed, groupBy, map, filter, flatten, uniq } from 'ramda';
 import { parse, format } from 'url';
 
-const initial = pipe(map(tweetLinks), flatten, uniq);
 const notTwitter = url => url.host !== 'twitter.com';
-const moveAllMinorsToOther = group => (length(group.links) < 5) ? 'other' : group.host;
-const groupByHost = pipe(
-  groupBy(prop('host')),
-  mapObjIndexed((value, key) => ({ host: key, links: value })),
-  compose(map(apply(objOf)), toPairs),
-  map(i => prop(head(keys(i)), i))
-);
-const constructResultObject = pipe(
-  mapObjIndexed(map(prop('links'))),
-  mapObjIndexed(flatten),
-  mapObjIndexed(map(format)), // format links
-  toPairs,
-  map(i => ({ host: i[0], links: i[1] }))
-);
+const obj2arr = pipe(toPairs, map(apply(objOf)));
 
-const getLinks = pipe(
-  initial,
-  map(parse), // parse links
-  filter(notTwitter),
-  groupByHost,
-  groupBy(moveAllMinorsToOther),
-  constructResultObject,
-  sortBy(group => group.host === 'other')
-);
+const extractLinks = pipe(map(tweetLinks), flatten, uniq);
+const filterTwitterLinks = pipe(map(parse), filter(notTwitter), map(format));
+const groupByHost = pipe(groupBy(pipe(parse, prop('host'))), obj2arr, map(pipe(values, flatten)));
+const moveMinorsToOther = pipe(groupBy( i => length(i) < 5 ? 'other' : parse(head(i)).host ), mapObjIndexed(flatten));
+const hostAndLinksObj = pipe(toPairs, map(zipObj(['host', 'links'])));
+const moveOtherToEnd = sortBy(group => group.host === 'other');
+const getLinks = pipe(extractLinks, filterTwitterLinks, groupByHost, moveMinorsToOther, hostAndLinksObj, moveOtherToEnd);
 
 export default getLinks;
