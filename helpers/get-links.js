cf74981857a0f19e5f5311416de200f0a816efa5
Vladimir Starkov <iamstarkov@gmail.com>
2015-10-29 17:00:15 +0100

    few code formatting love

--- helpers/get-links.js
+++ helpers/get-links.js
@@ -5,12 +5,37 @@ import { parse, format } from 'url';
 const notTwitter = url => url.host !== 'twitter.com';
 const obj2arr = pipe(toPairs, map(apply(objOf)));
 
-const extractLinks = pipe(map(tweetLinks), flatten, uniq);
-const filterTwitterLinks = pipe(map(parse), filter(notTwitter), map(format));
-const groupByHost = pipe(groupBy(pipe(parse, prop('host'))), obj2arr, map(pipe(values, flatten)));
-const moveMinorsToOther = pipe(groupBy( i => length(i) < 5 ? 'other' : parse(head(i)).host ), mapObjIndexed(flatten));
-const hostAndLinksObj = pipe(toPairs, map(zipObj(['host', 'links'])));
+const extractLinks = pipe(
+  map(tweetLinks),
+  flatten,
+  uniq);
+
+const filterTwitterLinks = pipe(
+  map(parse),
+  filter(notTwitter),
+  map(format));
+
+const groupByHost = pipe(
+  groupBy(pipe(parse, prop('host'))),
+  obj2arr,
+  map(pipe(values, flatten)));
+
+const moveMinorsToOther = pipe(
+  groupBy( i => length(i) < 5 ? 'other' : parse(head(i)).host ),
+  mapObjIndexed(flatten));
+
+const hostAndLinksObj = pipe(
+  toPairs,
+  map(zipObj(['host', 'links'])));
+
 const moveOtherToEnd = sortBy(group => group.host === 'other');
-const getLinks = pipe(extractLinks, filterTwitterLinks, groupByHost, moveMinorsToOther, hostAndLinksObj, moveOtherToEnd);
+
+const getLinks = pipe(
+  extractLinks,
+  filterTwitterLinks,
+  groupByHost,
+  moveMinorsToOther,
+  hostAndLinksObj,
+  moveOtherToEnd);
 
 export default getLinks;
