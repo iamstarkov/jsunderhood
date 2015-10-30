1240d4dfd1daf634cca58d6204aeb9a6a6f3f0e9
Vladimir Starkov <iamstarkov@gmail.com>
2015-10-30 18:31:12 +0100

    load helper fix

--- helpers/load.js
+++ helpers/load.js
@@ -2,14 +2,9 @@ import got from 'got';
 import { writeFile } from 'fs';
 import type from 'file-type';
 
-const cb = (err) => {
-  if (err) throw err;
-  console.log('It\'s saved!');
-}
-
 export default function load(imageUrl, targetName, cb) {
   got(imageUrl, { encoding: null })
     .then(({ body } = res) => ({ body, ext: type(body).ext }))
     .then(({ body, ext } = res) => writeFile(`${targetName}.${ext}`, body, cb))
-    .catch(cb)
+    .catch(cb);
 }
