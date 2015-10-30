5fe1a91e25681851cc57923c4f4c22077c91ffbe
Vladimir Starkov <iamstarkov@gmail.com>
2015-10-30 18:27:08 +0100

    lint and cleanup for author-render helper

--- helpers/author-render.js
+++ helpers/author-render.js
@@ -1,38 +1,26 @@
 import moment from 'moment';
-import { pipe, map, compose, reverse, filter, sort, flatten } from 'ramda';
+import { pipe, reverse, filter, groupBy, prop } from 'ramda';
 import numd from 'numd';
 import renderTweet from 'tweet.md';
 import getLinks from './get-links';
 import { html } from 'commonmark-helpers';
-
+import ungroupInto from './ungroup-into';
 
 moment.locale('ru');
 
-const getWeekday = date => moment(new Date(date)).format("dddd");
+const weekday = date => moment(new Date(date)).format('dddd');
 const tweetLink = (tweet) => `https://twitter.com/jsunderhood/status/${tweet.id_str}`;
 const tweetTime = (tweet) => moment(new Date(tweet.created_at)).format('H:mm');
 
-const separateByWeekdays = (state, item, i, arr) => {
-  var weekday = getWeekday(item.created_at);
-  if (!state.length) {
-    state.push({ weekday, tweets: [], ref: [] });
-  }
-  if (state[state.length - 1].weekday !== weekday) {
-    state.push({ weekday, tweets: [], ref: [] });
-  }
-  state[state.length - 1].tweets.push(item);
-  return state;
-};
-
-const d = input => moment(new Date(input)).format("D MMMM YYYY");
+const d = input => moment(new Date(input)).format('D MMMM YYYY');
 const tweetsUnit = numd('твит', 'твита', 'твитов');
 const capitalize = i => i.split('').map((item, i) => i === 0 ? item.toUpperCase() : item).join('');
 const filterTimeline = item => (item.text[0] !== '@') || (item.text.indexOf('@jsunderhood') === 0);
-const prepareTweets = compose(
-  arr => arr.reduce(separateByWeekdays, []),
+const prepareTweets = pipe(
+  reverse,
   filter(filterTimeline),
-  reverse
-);
+  groupBy(pipe(prop('created_at'), weekday)),
+  ungroupInto('weekday', 'tweets'));
 
 export default {
   d,
@@ -41,5 +29,5 @@ export default {
   tweetsUnit,
   render: pipe(renderTweet, html),
   tweetTime, tweetLink,
-  getLinks
+  getLinks,
 };
