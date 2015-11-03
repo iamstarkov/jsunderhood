8b5ffc804a64e03f56ba7a3563f6eabaa3d69084
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-03 01:33:53 +0100

    remove obsolete scripts

--- dump-authors-original-media.js
+++
@@ -1,6 +0,0 @@
-import authors from './authors';
-import loadMedia from './helpers/load-media';
-
-authors
-  .map(author => author.username)
-  .forEach(author => { loadMedia(author, 'dump/images/original', author); });
