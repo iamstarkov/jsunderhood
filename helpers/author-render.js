964d3e0f452eb8f4642094cf8f3559937f7407c7
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-03 05:05:22 +0100

    add prev/next authors

--- helpers/author-render.js
+++ helpers/author-render.js
@@ -1,6 +1,7 @@
 import moment from 'moment';
-import { pipe, reverse, filter, groupBy, prop, converge,
-  path, map, head, split, nth, replace, toUpper, tail, concat } from 'ramda';
+import { pipe, reverse, filter, groupBy, prop, converge, inc, dec, length,
+  findIndex, propEq, path, map, head, split, nth, replace, toUpper, tail,
+  concat } from 'ramda';
 import numd from 'numd';
 import renderTweet from 'tweet.md';
 import getLinks from './get-links';
@@ -9,6 +10,7 @@ import ungroupInto from './ungroup-into';
 import unidecode from 'unidecode';
 import trimTag from 'trim-html-tag';
 import { parse } from 'url';
+import authors from '../authors';
 
 const getQuotedUser = pipe(
   path(['entities', 'urls']),
@@ -24,6 +26,18 @@ const weekday = date => moment(new Date(date)).format('dddd');
 const tweetLink = (tweet) => `https://twitter.com/jsunderhood/status/${tweet.id_str}`;
 const tweetTime = (tweet) => moment(new Date(tweet.created_at)).format('H:mm');
 
+const authorsToPost = filter(author => author.post !== false, authors);
+
+const authorIndex = author => findIndex(propEq('username', author.username))(authorsToPost);
+const isFirstAuthor = author => authorIndex(author) === dec(length(authorsToPost));
+const isLastAuthor = author => author.username === prop('username', head(authorsToPost));
+const nextAuthor = author => {
+  if (!isLastAuthor(author)) return nth(dec(authorIndex(author)), authorsToPost);
+};
+const prevAuthor = author => {
+  if (!isFirstAuthor(author)) return nth(inc(authorIndex(author)), authorsToPost);
+};
+
 const d = input => moment(new Date(input)).format('D MMMM YYYY');
 const tweetsUnit = numd('твит', 'твита', 'твитов');
 const capitalize = converge(concat, [pipe(head, toUpper), tail]);
@@ -34,7 +48,6 @@ const prepareTweets = pipe(
   groupBy(pipe(prop('created_at'), weekday)),
   ungroupInto('weekday', 'tweets'));
 
-
 export default {
   d,
   prepareTweets,
@@ -42,6 +55,7 @@ export default {
   tweetsUnit,
   getQuotedUser,
   unidecode,
+  prevAuthor, nextAuthor,
   render: pipe(renderTweet, html, trimTag),
   tweetTime, tweetLink,
   getLinks,
