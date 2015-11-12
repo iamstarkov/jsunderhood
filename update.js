dd39ff081cc00267333db658404c7ebfb48ab6af
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-12 03:18:01 +0100

    implement mentions dump

--- update.js
+++ update.js
@@ -3,15 +3,16 @@ import authors from './authors';
 import log from './helpers/log';
 import moment from 'moment';
 import { outputFile, outputJSON } from 'fs-extra';
-import { dissoc, map, head, prop } from 'ramda';
+import { concat, reverse, last, dissoc, map, head } from 'ramda';
 
 import saveMedia from './helpers/save-media';
 import getTweets from 'get-tweets';
 import getInfo from 'get-twitter-info';
 import getFollowers from 'get-twitter-followers';
+import twitterMentions from 'twitter-mentions';
 
-const username = prop('username', head(authors));
-const first = prop('first', head(authors));
+const currentAuthor = head(authors);
+const { first, username } = currentAuthor;
 
 const spaces = 2;
 
@@ -44,4 +45,15 @@ getFollowers(tokens, 'jsunderhood', (err, followersWithStatuses) => {
   });
 });
 
-outputFile('./dump/.timestamp', moment().unix(), () => {});
+const firstTweet = last(currentAuthor.tweets);
+const lastMention = last(currentAuthor.mentions);
+const sinceId = (lastMention || firstTweet).id_str;
+twitterMentions(tokens, sinceId, (err, mentionsRaw) => {
+  if (err) throw err;
+  const mentions = concat(currentAuthor.mentions, reverse(mentionsRaw));
+  outputJSON(`./dump/${username}-mentions.json`, { mentions }, { spaces }, saveErr => {
+    log(`${saveErr ? '✗' : '✓'} ${username}’s mentions`);
+  });
+});
+
+outputFile('./dump/.timestamp', moment().unix() , () => {});
