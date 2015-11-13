9a8dbb97eac43b56992caa84d32f524d0744717c
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-13 03:33:23 +0100

    update updatejs getTweets and getMentions

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 import log from './helpers/log';
-import { outputFile, outputJSON } from 'fs-extra';
+import { outputFile } from 'fs-extra';
 import { isEmpty, concat, reverse, last, dissoc, map, head } from 'ramda';
 import moment from 'moment';
 import dec from 'bignum-dec';
@@ -17,19 +17,18 @@ import ensureFilesForFirstUpdate from './helpers/ensure-author-files';
 import getAuthorArea from './helpers/get-author-area';
 import saveAuthorArea from './helpers/get-author-area';
 
-const author = head(authors);
-const { first, username } = author;
-
-const spaces = 2;
+const { first, username } = head(authors);
 
 ensureFilesForFirstUpdate(username);
 
-const authorTweets = getAuthorArea(author, 'tweets').tweets || [];
-const tweetsSinceId = isEmpty(authorTweets) ? dec(first) : last(authorTweets).id_str;
-getTweets(tokens, 'jsunderhood', tweetsSinceId, (err, tweetsRaw) => {
+const tweets = getAuthorArea(username, 'tweets').tweets || [];
+const mentions = getAuthorArea(username, 'mentions').mentions || [];
+
+const tweetsSinceId = isEmpty(tweets) ? dec(first) : last(tweets).id_str;
+getTweets(tokens, 'jsunderhood', tweetsSinceId, (err, newTweetsRaw) => {
   if (err) throw err;
-  const tweets = concat(authorTweets, reverse(tweetsRaw));
-  saveAuthorArea(username, 'tweets', { tweets });
+  const concattedTweets = concat(tweets, reverse(newTweetsRaw));
+  saveAuthorArea(username, 'tweets', { tweets: concattedTweets });
 });
 
 getInfo(tokens, 'jsunderhood', (err, info) => {
@@ -48,15 +47,11 @@ getFollowers(tokens, 'jsunderhood', (err, followersWithStatuses) => {
   saveAuthorArea(username, 'followers', { followers });
 });
 
-const firstTweet = last(currentAuthor.tweets);
-const lastMention = last(currentAuthor.mentions);
-const sinceId = (lastMention || firstTweet).id_str;
-twitterMentions(tokens, sinceId, (err, mentionsRaw) => {
+const mentionsSinceId = isEmpty(mentions) ? first : last(mentions).id_str;
+twitterMentions(tokens, mentionsSinceId, (err, newMentionsRaw) => {
   if (err) throw err;
-  const mentions = concat(currentAuthor.mentions, reverse(mentionsRaw));
-  outputJSON(`./dump/${username}-mentions.json`, { mentions }, { spaces }, saveErr => {
-    log(`${saveErr ? '✗' : '✓'} ${username}’s mentions`);
-  });
+  const concattedMentions = concat(mentions, reverse(newMentionsRaw));
+  saveAuthorArea(username, 'mentions', { mentions: concattedMentions });
 });
 
 outputFile('./dump/.timestamp', moment().unix(), err => {
