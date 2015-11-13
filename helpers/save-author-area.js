0b06277d80ea021b72c86399990a49a6376037c5
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-13 04:56:33 +0100

    ¯\_(ツ)_/¯

--- helpers/save-author-area.js
+++ helpers/save-author-area.js
@@ -4,7 +4,7 @@ import log from './log';
 const spaces = 2;
 
 export default function saveAuthorArea(username, area, content) {
-  outputJSON(`../dump/${username}-${area}.json`, content, { spaces }, err => {
+  outputJSON(`./dump/${username}-${area}.json`, content, { spaces }, err => {
     log(`${err ? '✗' : '✓'} ${username}’s ${area}`);
   });
 }
