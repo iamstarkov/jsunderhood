a8113e5e376c8b0da5c048d20fa45d2defe0084f
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-16 19:30:01 +0100

    fix problem for initial case

--- dump/index.js
+++ dump/index.js
@@ -3,10 +3,10 @@ import authors from '../authors';
 import getAuthorArea from '../helpers/get-author-area';
 
 const saturate = author => merge(author, {
-  info: getAuthorArea(author.username, 'info'),
-  tweets: getAuthorArea(author.username, 'tweets').tweets,
-  media: getAuthorArea(author.username, 'media'),
-  mentions: getAuthorArea(author.username, 'mentions').mentions,
+  info: getAuthorArea(author.username, 'info') || {},
+  tweets: getAuthorArea(author.username, 'tweets').tweets || [],
+  media: getAuthorArea(author.username, 'media') || {},
+  mentions: getAuthorArea(author.username, 'mentions').mentions || [],
 });
 
 export default map(saturate, authors);
