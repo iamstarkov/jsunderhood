7c9f460cd9187f22e263cf6e71446c9e5b643ec6
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-16 19:32:44 +0100

    fix for helpers

--- helpers/get-author-area.js
+++ helpers/get-author-area.js
@@ -1,5 +1,9 @@
 import { readJsonSync } from 'fs-extra';
 
 export default function getAuthorArea(username, area) {
-  return readJsonSync(`./dump/${username}-${area}.json`, { throws: false });
+  try {
+    return readJsonSync(`./dump/${username}-${area}.json`);
+  } catch (e) {
+    return {};
+  }
 }
--- helpers/last-updated.js
+++ helpers/last-updated.js
@@ -1,9 +1,16 @@
 import { readFileSync } from 'fs';
-import { unix } from 'moment';
+import moment from 'moment';
 
 const cwd = process.cwd();
-const timestamp = parseInt(readFileSync(`${cwd}/dump/.timestamp`, `utf8`), 10);
-const date = unix(timestamp);
+const timestamp = (() => {
+  try {
+    parseInt(readFileSync(`${cwd}/dump/.timestamp`, `utf8`), 10);
+  } catch (e) {
+    return parseInt(moment.unix(), 10);
+  }
+})();
+
+const date = moment.unix(timestamp);
 
 function lastUpdated() {
   return date.locale('ru').format('D MMMM в H:mm');
