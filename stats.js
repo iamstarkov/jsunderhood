7655053eab48d543e2fd612f4052e5d1f3318aed
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-16 22:17:14 +0100

    small refactoring

--- stats.js
+++ stats.js
@@ -1,21 +1,22 @@
 import stats from 'tweets-stats';
-import maxValues from 'max-values'
+import maxValues from 'max-values';
+import { merge } from 'ramda';
 
 const getStatsPerAuthor = authors =>
   authors
-    .map(author => (Object.assign({}, author, { followers: author.info.followers_count })))
-    .map((author, i, arr) => (Object.assign({}, author, {
+    .map(author => merge(author, { followers: author.info.followers_count }))
+    .map((author, i, arr) => merge(author, {
       gainedFollowers: arr[i + 1]
         ? (author.followers - arr[i + 1].followers)
-        : author.followers
-    })))
-    .map(author => Object.assign({}, author, stats(author.tweets)))
+        : author.followers,
+    }))
+    .map(author => merge(author, stats(author.tweets)));
 
 export default function getStats(authors) {
   return maxValues(getStatsPerAuthor(authors), [
     'tweets', 'gainedFollowers',
     'own.total', 'replies.total', 'retweets.total',
     'favorited.total', 'favorited.average',
-    'retweeted.total', 'retweeted.average'
+    'retweeted.total', 'retweeted.average',
   ]);
-};
+}
