2f07c443df7fda8cd830a44240bc49c55a1fabb2
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-16 14:13:16 +0100

    proper destructuring

--- helpers/save.js
+++ helpers/save.js
@@ -4,12 +4,12 @@ import { writeFile } from 'fs';
 
 export default function save(image, path, cb) {
   got(image, { encoding: null })
-    .then(({ body } = res) => ({ body, ext: type(body).ext }))
-    .then(({ body, ext } = res) => {
+    .then(({ body }) => ({ body, ext: type(body).ext }))
+    .then(({ body, ext }) => {
       writeFile(`./dump/${path}.${ext}`, body, err => {
         if (err) return cb(err);
         cb(null, `${path}.${ext}`.replace('dump/', ''));
-      })
+      });
     })
     .catch(cb);
 }
