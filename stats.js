9efdfa3dba8589485917f9f47c68404c642d315d
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-18 03:32:02 +0100

    wip

--- stats.js
+++ stats.js
@@ -1,34 +1,21 @@
 import stats from 'tweets-stats';
 import maxValues from 'max-values';
 import { merge } from 'ramda';
-import { difference } from 'underscore';
+import getGainedFollowers from './helpers/get-gained-followers';
+import getDiffFollowers from './helpers/get-diff-followers';
 
-const getFollowerStats = (prevAuthor, curAuthor) => ({
-  followersGained: difference(curAuthor, prevAuthor).length,
-  followersLost: difference(prevAuthor, curAuthor).length
-});
-
-const getStatsPerAuthor = authors =>
-  authors
-    .map(author => merge(author, {
-      followersCount: author.info.followers_count,
-      followers: author.followers.map(follower => follower.id)
-    }))
-    .map((author, i, arr) => merge(author,
-      arr[i + 1]
-        ? merge({followersDiff: author.followersCount - arr[i + 1].followersCount},
-        getFollowerStats(arr[i + 1].followers, author.followers))
-        : {followersGained: author.followersCount, followersLost: 0, followersDiff: author.followersCount}
-    ))
-    .map(author => merge(author, {
-      followersValid: Math.abs(author.followersGained - author.followersLost) === Math.abs(author.followersDiff)
-    }))
+function getStatsPerAuthor(authors) {
+  return authors
+    .map(author => merge(author, { followers: author.info.followers_count }))
+    .map(author => merge(author, { gainedFollowers: getGainedFollowers(author.username) }))
+    .map(author => merge(author, { diffFollowers: getDiffFollowers(author.username) }))
     .map(author => merge(author, stats(author.tweets)));
+}
 
 export default function getStats(authors) {
   if (!authors || authors.length === 0) return;
   return maxValues(getStatsPerAuthor(authors), [
-    'tweets', 'followersDiff', 'followersGained', 'followersLost', 'followersValid',
+    'tweets', 'gainedFollowers',
     'own.total', 'replies.total', 'retweets.total',
     'favorited.total', 'favorited.average',
     'retweeted.total', 'retweeted.average',
