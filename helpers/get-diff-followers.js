39aa1ff84760e99e797a079b01e692ea78914064
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-18 03:25:43 +0100

    update get-diff-followers implementation

--- helpers/get-diff-followers.js
+++ helpers/get-diff-followers.js
@@ -4,14 +4,16 @@ import R from 'ramda';
 import diff from 'lodash.difference';
 
 const prev = author => (authors[R.inc(R.findIndex(R.propEq('username', author), authors))] || {}).username;
-const followers = author => R.map(R.prop('id_str'), getAuthorArea(author, 'followers').followers);
+const followers = author => R.map(R.prop('id_str'), (getAuthorArea(author, 'followers').followers || []));
 
 // getDiffFollowers :: String -> Object
 export default function getDiffFollowers(author) {
   const currentFollowers = followers(author);
   const previousFollowers = followers(prev(author));
-  return {
-    gain: R.length(diff(currentFollowers, previousFollowers)),
-    loss: R.length(diff(previousFollowers, currentFollowers)),
-  };
+  if (R.length(previousFollowers) > 0) {
+    return {
+      gain: R.length(diff(currentFollowers, previousFollowers)),
+      loss: R.length(diff(previousFollowers, currentFollowers)),
+    };
+  }
 }
