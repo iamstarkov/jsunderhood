a88e0d53c6446af14247f0f2100f999fdef9beb8
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-18 02:59:35 +0100

    update getDiffFollowers signature

--- helpers/get-diff-followers.js
+++ helpers/get-diff-followers.js
@@ -6,7 +6,7 @@ import diff from 'lodash.difference';
 const prev = author => (authors[R.inc(R.findIndex(R.propEq('username', author), authors))] || {}).username;
 const followers = author => R.map(R.prop('id_str'), getAuthorArea(author, 'followers').followers);
 
-// getGainedFollowers :: String -> Number
+// getDiffFollowers :: String -> Object
 export default function getDiffFollowers(author) {
   const currentFollowers = followers(author);
   const previousFollowers = followers(prev(author));
