b7222365590a0ad506e503663f59848db0e9a11b
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-18 02:07:29 +0100

    implement getGainedFollowers

--- helpers/get-gained-followers.js
+++ helpers/get-gained-followers.js
@@ -1,3 +1,11 @@
+import getAuthorArea from './get-author-area';
+import authors from '../authors';
+import R from 'ramda';
+
+const prev = author => (authors[R.inc(R.findIndex(R.propEq('username', author), authors))] || {}).username;
+const followers = author => getAuthorArea(author, 'info').followers_count || 0;
+
+// getGainedFollowers :: String -> Number
 export default function getGainedFollowers(author) {
-  return author.info.followers_count - prev(author).info.followers_count);
+  return followers(author) - followers(prev(author));
 }
