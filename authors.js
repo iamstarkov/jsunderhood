098a7546bf3cf7c162158b8336f73eef7becb21b
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2016-04-24 22:26:33 +0200

    Update authors.js

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@
 
 export default [
   // post: false
-  { username: 'blackrabbit99',   start: '18 Apr 2016', first: '721962420391030784', post: false },
+  { username: 'blackrabbit99',   start: '18 Apr 2016', first: '721962420391030784' },
   { username: 'roman01la-2',     start: '11 Apr 2016', first: '719472523876941824' },
   { username: 'xanf_ua',         start: '28 Mar 2016', first: '714343484950491136' },
   { username: 'iadramelk',       start: '21 Mar 2016', first: '711964591291539456' },
