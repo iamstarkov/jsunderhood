d81c5e450f1a71e7f4c5184daa51a9f573706595
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2016-04-25 12:03:28 +0200

    add @vvscode

--- authors.js
+++ authors.js
@@ -2,6 +2,7 @@
 
 export default [
   // post: false
+  { username: 'vvscode',         start: '25 Apr 2016', first: '724530880593702912', post: false },
   { username: 'blackrabbit99',   start: '18 Apr 2016', first: '721962420391030784' },
   { username: 'roman01la-2',     start: '11 Apr 2016', first: '719472523876941824' },
   { username: 'xanf_ua',         start: '28 Mar 2016', first: '714343484950491136' },
